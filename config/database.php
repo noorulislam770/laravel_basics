48f2c68f30f4611656be0678893ea2402dccf5a9
Noor Ul Islam <noorulislam770@gmail.com>
2024-01-09 21:12:18 +0500

    changed database credentials and added route to run migratins on aws -v2 change database credentials in database.php again with the database name from RDS- udoing various thing

--- config/database.php
+++ config/database.php
@@ -45,12 +45,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-
             'host' => env('RDS_HOSTNAME', '127.0.0.1'),
             'port' => env('RDS_PORT', '3306'),
-            'database' => env('RDS_DB_DATABASE', 'awseb-e-nnmjg3v8c9-stack-awsebrdsdatabase-cdmmj8jortkl'),
-            'username' => env('RDS_USERNAME', 'root'),
-            'password' => env('RDS_PASSWORD', 'Patengel1!'),
+            'database' => env('RDS_DB_NAME', 'forge'),
+            'username' => env('RDS_USERNAME', 'forge'),
+            'password' => env('RDS_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
